d1052875538d7d0b6b5c07b81603ff1ff0097ef2
Sat1067 <geovanny1067@hotmail.com>
2017-12-01 22:38:56 -0500

    Adicion de DayPicker para el proyecto 100% funcional

---
+++ ThePlaning/ClientApp/components/general_components/form_components/date-picker/date-piker.tsx
@@ -0,0 +1,27 @@
+﻿// A '.tsx' file enables JSX support in the TypeScript compiler, 
+// for more information see the following page on the TypeScript wiki:
+// https://github.com/Microsoft/TypeScript/wiki/JSX
+
+import * as React from 'react';
+import * as  moment from 'moment'; 
+import DatePicker from 'react-datepicker';
+
+import './react-datepicker.css';
+
+interface Props {
+    selected: any;
+    onChange: any;
+    id: any;
+}
+
+export default class DayPicker extends React.Component<Props, {}> {
+
+    render() {
+        return (
+            <DatePicker id={this.props.id} selected={this.props.selected} onChange={this.props.onChange} />
+        )
+    }
+} 
+
+
+
